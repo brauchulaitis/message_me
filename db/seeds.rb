ab1c1f71056d5984659026e838e680ce7663b807
brauchulaitis <oskars.braucs@gmail.com>
2023-04-17 13:09:07 +0300

    Build users resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(username: "Oskars", password: "password")
+User.create(username: "Aragorn", password: "password")
+User.create(username: "Frodo", password: "password")
+User.create(username: "Gandalf", password: "password")
+User.create(username: "Legolas", password: "password")
