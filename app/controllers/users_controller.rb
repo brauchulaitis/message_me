799c7815c99c3dbfbfc5ac3dfaec84837f4451f0
brauchulaitis <oskars.braucs@gmail.com>
2023-04-26 10:01:03 +0300

    Login update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,18 @@ class UsersController < ApplicationController
   before_action :logged_in_redirect
 
   def new
+    @user = User.new
+  end
 
+  def create
+    @user = User.new(user_params)
+    if user.save
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome to MessageMe #{@user.username}, you have successfully signed up"
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   private
